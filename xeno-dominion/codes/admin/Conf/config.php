70a1df490c86f5277d557ae91882f9d5473e468c
luolisave@gmail.com <luolisave@gmail.com@73d3f71f-334f-4d26-b745-6e3f5db6f39b>
2011-10-10 17:23:48 +0000

    Update DB user name and password

--- xeno-dominion/codes/admin/Conf/config.php
+++ xeno-dominion/codes/admin/Conf/config.php
@@ -4,8 +4,8 @@ return array(
 	'DB_TYPE'=> 'mysql', // 数据库类型
 	'DB_HOST'=> 'localhost', // 数据库朋务器地址
 	'DB_NAME'=>'recrazy_xeno', // 数据库名称
-	'DB_USER'=>'root', // 数据库用户名
-	'DB_PWD'=>'', // 数据库密码
+	'DB_USER'=>'recrazy', // 数据库用户名
+	'DB_PWD'=>'recrazy', // 数据库密码
 	'DB_PORT'=>'3306', // 数据库端口
 	'DB_PREFIX'=>'ry_', // 数据表前缀
 	
