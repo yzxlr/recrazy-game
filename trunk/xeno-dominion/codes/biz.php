88af4d4436d181abad95cd83caf9b0e6ac649fbe
luolisave@gmail.com <luolisave@gmail.com@73d3f71f-334f-4d26-b745-6e3f5db6f39b>
2011-10-21 00:50:02 +0000

    biz app init

---
+++ trunk/xeno-dominion/codes/biz.php
@@ -0,0 +1,26 @@
+<?php
+//开发时的一半配置
+//http://www.ciphp.com/2010/05/436
+
+//jack调试用 （另外的调试设置在 admin/conf/config.php中的末尾）
+define('NO_CACHE_RUNTIME',True);//不生成runtime文件
+//firePHP
+require_once('./public/FirePHPCore/FirePHP.class.php');
+$firephp = FirePHP::getInstance(true);
+$var = array('i'=>10, 'j'=>20);
+//$firephp->log($var, 'Iterators');
+require_once('./public/FirePHPCore/fb.php');
+//FB::send("These two messages came from /admin.php");
+//firePHP ends
+
+
+// 定丿ThinkPHP框架路徂
+define('THINK_PATH', './ThinkPHP/');
+//定丿项目名称和路徂
+define('APP_NAME', 'Mybiz');
+define('APP_PATH', './biz');
+// 加载框架入口文件
+require(THINK_PATH."/ThinkPHP.php");
+//实例化一个网站应用实例
+App::run();
+?>
---
+++ trunk/xeno-dominion/codes/biz/Lib/Action/CommonAction.class.php
@@ -0,0 +1,58 @@
+<?php
+// 本文档自动生成，仅供测试运行
+class CommonAction extends Action
+{
+	/**
+	*  所有的公用的东西都可以放在这里。
+	*   $this->assign('var',$value);都可以用。下面的方法都会调用这个initialize里面的内  容
+	*/
+	
+	protected $user = array(); 
+    public function _initialize(){
+		//0. Initialization
+		//0.1 Global variables
+		$http = (isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS']!='off')?'https://':'http://';  
+		$port = $_SERVER["SERVER_PORT"]==80?'':':'.$_SERVER["SERVER_PORT"];  
+		$url = $http.$_SERVER['SERVER_NAME'].$port;//.$_SERVER["REQUEST_URI"];
+		$this->assign("SITE_URL",$url);
+		//define("SITE_URL",$url);
+		
+		//1 Validate User (only admin can login at here
+		if(!empty($_SESSION["user"])){
+			$this->user = $_SESSION["user"];
+			if($this->user["role"]==0){
+				$this->assign("user",$this->user);
+			}else{
+				$this->assign("jumpUrl","/index.php?s=Public/login");
+				$this->error('You are not administration user!');
+			}
+		}else{
+			$this->assign("jumpUrl","/index.php?s=Public/login");
+			$this->error('Please login first!');
+			//$this->redirect('Public/login', array(), 3, 'Please login first!');
+		}
+		
+	}
+	
+	
+	/*
+	 *  这里暂时hardcode
+	**/
+	public function rbac($user_id, $user_action){
+		return true;
+	}
+	
+	/*
+	 *  这里暂时hardcode
+	**/
+	public function getUserRoles(){
+		$user_role = array();
+		$user_role[2] = array("id"=>100,"name"=>"user");
+		$user_role[1] = array("id"=>10,"name"=>"master");
+		$user_role[0] = array("id"=>0,"name"=>"admin");
+		
+		return $user_role;
+	}
+
+}
+?>
