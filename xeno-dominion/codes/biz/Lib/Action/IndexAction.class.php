c27a5fd8496814af3f33f942b65fef0947250bbe
luolisave@gmail.com <luolisave@gmail.com@73d3f71f-334f-4d26-b745-6e3f5db6f39b>
2011-10-31 03:09:40 +0000

    Calender/task is done.

--- xeno-dominion/codes/biz/Lib/Action/IndexAction.class.php
+++ xeno-dominion/codes/biz/Lib/Action/IndexAction.class.php
@@ -9,6 +9,43 @@ class IndexAction extends CommonAction
     */
     public function index()
     {
+        //0. Initialization
+		//0.1 Global variables
+		//0.2 Define Tables
+		$tb_task = M("task");
+		//0.4 Condition
+		$condition = array();
+		//0.9 massenger
+		$msg = array("title"=>"Biz Index"); 
+		//1. Table task
+		//1.1 add
+		//1.4 Select
+		date_default_timezone_set("America/Toronto");
+		$today = date("Y-m-d");
+		$today .= " 00:00:00";
+		$tomorrow = date("Y-m-d",strtotime("+1 day"));
+		$tomorrow .= " 00:00:00";
+		$tomorrow_one = date("Y-m-d",strtotime("+2 day"));
+		$tomorrow_one .= " 00:00:00";
+		$nextweek = date("Y-m-d",strtotime("+8 day"));
+		$nextweek .= " 00:00:00";
+		//$nextweek .= " 00:00:00";
+		$condition = array("user_id"=>$this->user["uid"], "status"=>1, "time"=>array(array("elt",$nextweek),array("egt",$today)));
+		$data["tb_task"] = $tb_task -> where($condition) ->order("time ASC") -> select();
+		
+		//today
+		$condition = array("user_id"=>$this->user["uid"], "status"=>1, "time"=>array(array("lt",$tomorrow),array("egt",$today)));
+		$data["tb_task_today"] = $tb_task -> where($condition) ->order("time ASC") -> select();
+		//tomorrow
+		$condition = array("user_id"=>$this->user["uid"], "status"=>1, "time"=>array(array("lt",$tomorrow_one),array("egt",$tomorrow)));
+		$data["tb_task_tomorrow"] = $tb_task -> where($condition) ->order("time ASC") -> select();
+		//nextweek
+		$condition = array("user_id"=>$this->user["uid"], "status"=>1, "time"=>array(array("elt",$nextweek),array("egt",$tomorrow_one)));
+		$data["tb_task_nextweek"] = $tb_task -> where($condition) ->order("time ASC") -> select();
+		
+		//10. Display
+		$this->assign("data",$data);
+		$this->assign("msg",$msg);
         $this->display();
     }
 	
