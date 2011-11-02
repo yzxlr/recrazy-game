13e9709e63633d2f61042bbd08e4fb06645ac927
luolisave@gmail.com <luolisave@gmail.com@73d3f71f-334f-4d26-b745-6e3f5db6f39b>
2011-11-02 01:58:01 +0000

    init product action

---
+++ trunk/xeno-dominion/codes/admin/Lib/Action/ProductAction.class.php
@@ -0,0 +1,31 @@
+<?php
+// 本文档自动生成，仅供测试运行
+class ProductAction extends CommonAction
+{
+    /**
+    +----------------------------------------------------------
+    * 默认操作
+    +----------------------------------------------------------
+    */
+    public function index()
+    {
+        //0. Initialization
+		//0.1 Global variables
+		$user = $_SESSION["user"];
+		//0.2 Define Tables
+		$tb_product = M("product");
+		//0.9 massenger
+		$msg = array("title"=>"List Products"); 
+		
+		//1. Table product
+		$data["tb_product"] = $tb_product -> select();
+		
+		//10. Display
+		$this->assign("data",$data);
+		$this->assign("msg",$msg);
+        $this->display();
+    }
+
+
+}
+?>
